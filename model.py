b4da7a425ba896e7a52a5808292baabfb3a4e855
Peter Hwang <peterhwang.inbev@gmail.com>
2017-04-30 16:40:31 -0400

    initial commit

---
+++ model.py
@@ -0,0 +1,50 @@
+import csv
+import matplotlib.image as mpimg
+import numpy as np
+
+lines = []
+with open('../data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = '../data/IMG/' + filename
+    image = mpimg.imread(current_path)
+    image_flipped = np.fliplr(image)
+    images.append(image)
+    images.append(image_flipped)
+    measurement = float(line[3])
+    measurement_flipped = -measurement
+    measurements.append(measurement)
+    measurements.append(measurement_flipped)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Convolution2D(24,5,5, subsample=(2,2),activation='relu'))
+model.add(Convolution2D(36,5,5, subsample=(2,2),activation='relu'))
+model.add(Convolution2D(48,5,5, subsample=(2,2),activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+
+model.save('model.h5')
